b781838b8dd5e36fecacf31bb3b48696fa51f38a
Geovana Miranda <182geovana@gmail.com>
2025-08-29 16:37:01 -0300

    feat: adiciona pasta public

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+if (!array_key_exists("PATH_INFO", $_SERVER) || $path === "/") {
+    require_once __DIR__ . "/../pages/home.php";
+} elseif ($path === "/login") {
+    require_once __DIR__ . "/../pages/login.php";
+} elseif ($path === "/register") {
+    require_once __DIR__ . "/../pages/register.php";
+} elseif ($path === "/details") {
+    require_once __DIR__ . "/../pages/details.php";
+} elseif ($path === "/newitem") {
+    require_once __DIR__ . "/../pages/newitem.php";
+} elseif ($path === "/edit-item") {
+    require_once __DIR__ . "/../pages/edit-item.php";
+} else {
+    require_once __DIR__ . "/../pages/home.php";
+}
